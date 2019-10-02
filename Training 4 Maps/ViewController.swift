03fc6416fafd2fd74ee5ebc52330cbe8d190868d
yudha <yudhapurbajagad@gmail.com>
2019-10-02 13:07:50 +0700

    Initial Commit

---
+++ Training 4 Maps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Training 4 Maps
+//
+//  Created by yudha on 02/10/19.
+//  Copyright © 2019 yudha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
